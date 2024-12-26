5ec8a0dd64e843c46f74d96009eef654360129fe
Sara Maria Custodio dos Santos <contatosarams@gmail.com>
2024-12-25 21:46:31 -0300

    linha 11 e 13: alteração na formatação do desconto
    Alterado a formatação do desconto na mensagem final para que este aparece com o símbolo de porcentagem respeitando ao menos um casa decimal.

--- src/PrecoDesconto.java
+++ src/PrecoDesconto.java
@@ -8,9 +8,9 @@ public class PrecoDesconto {
 
         String informaPrecoFinal = """
                 O preço original é R$ %.2f,
-                o desconto é de %.2f,
+                o desconto é de %.1f%%,
                 o preço final com desconto é R$ %.2f
-                """.formatted(precoOriginal, percentualDesconto, aplicaDescontoPrecoOriginal);
+                """.formatted(precoOriginal, percentualDesconto * 100, aplicaDescontoPrecoOriginal);
         System.out.println(informaPrecoFinal);
     }
 }
